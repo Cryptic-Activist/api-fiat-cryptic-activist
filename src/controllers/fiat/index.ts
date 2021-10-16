07e799c3a455bfc447cbbc461ccee3434d5c772d
Davi Silva <davi.cunha.silva.ca@gmail.com>
2021-10-16 22:09:53 +0200

    feat(test): test
    test

---
+++ src/controllers/fiat/index.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { getFiat } from 'cryptic-base';
+
+export async function getFiatController(
+  req: Request,
+  res: Response,
+): Promise<Response> {
+  try {
+    const { fiatSymbol } = req.query;
+
+    const fiat = await getFiat({ symbol: fiatSymbol });
+
+    return res.status(200).send({
+      status_code: 200,
+      results: fiat,
+      errors: [],
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status_code: 500,
+      results: {},
+      errors: [err.message],
+    });
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Application } from 'express';
 
+import fiat from './fiat';
 import fiats from './fiats';
 
 export default (app: Application): void => {
+  app.use('/fiat', fiat);
   app.use('/fiats', fiats);
 };

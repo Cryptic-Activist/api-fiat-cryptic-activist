70804736d645f6b2498eaf0ca7a05f5137ea9400
Davi Silva <davi.cunha.silva.ca@gmail.com>
2023-01-15 10:08:07 -0500

    chore: refactoring

--- src/controllers/fiat/index.ts
+++ src/controllers/fiat/index.ts
@@ -1,29 +1,29 @@
 import { Request, Response } from 'express';
-import { getFiat } from 'cryptic-base';
+import { getFiat } from 'base-ca';
 
 export async function getFiatController(
-  req: Request,
-  res: Response,
+	req: Request,
+	res: Response,
 ): Promise<Response> {
-  try {
-    const { fiatSymbol } = req.query;
+	try {
+		const { fiatSymbol } = req.query;
 
-    const fiat = await getFiat({ symbol: fiatSymbol });
+		const fiat = await getFiat({ symbol: fiatSymbol as string });
 
-    return res.status(200).send({
-      status_code: 200,
-      results: {
-        id: fiat.id,
-        symbol: fiat.symbol,
-        name: fiat.name,
-      },
-      errors: [],
-    });
-  } catch (err) {
-    return res.status(500).send({
-      status_code: 500,
-      results: {},
-      errors: [err.message],
-    });
-  }
+		return res.status(200).send({
+			status_code: 200,
+			results: {
+				id: fiat.id,
+				symbol: fiat.symbol,
+				name: fiat.name,
+			},
+			errors: [],
+		});
+	} catch (err) {
+		return res.status(500).send({
+			status_code: 500,
+			results: {},
+			errors: [err.message],
+		});
+	}
 }
--- src/controllers/fiats/index.ts
+++ src/controllers/fiats/index.ts
@@ -1,100 +1,100 @@
 import { Request, Response } from 'express';
-import { getFiats, createFiat, getFiat } from 'cryptic-base';
+import { getFiats, createFiat, getFiat } from 'base-ca';
 
 import fiatsJson from '../../../fiats.json';
 
 export async function index(req: Request, res: Response): Promise<Response> {
-  try {
-    const fiats = await getFiats(null);
+	try {
+		const fiats = await getFiats();
 
-    return res.status(200).send({
-      status_code: 200,
-      results: fiats,
-      errors: [],
-    });
-  } catch (err) {
-    return res.status(500).send({
-      status_code: 500,
-      results: [],
-      errors: [err.message],
-    });
-  }
+		return res.status(200).send({
+			status_code: 200,
+			results: fiats,
+			errors: [],
+		});
+	} catch (err) {
+		return res.status(500).send({
+			status_code: 500,
+			results: [],
+			errors: [err.message],
+		});
+	}
 }
 
 export async function createFiatController(
-  req: Request,
-  res: Response,
+	req: Request,
+	res: Response,
 ): Promise<Response> {
-  try {
-    const { name, symbol } = req.body;
+	try {
+		const { name, symbol } = req.body;
 
-    const newFiat = await createFiat({
-      name,
-      symbol,
-    });
+		const newFiat = await createFiat({
+			name,
+			symbol,
+		});
 
-    return res.status(200).send({
-      status_code: 200,
-      results: newFiat,
-      errors: [],
-    });
-  } catch (err) {
-    return res.status(500).send({
-      status_code: 500,
-      results: {},
-      errors: [err.message],
-    });
-  }
+		return res.status(200).send({
+			status_code: 200,
+			results: newFiat,
+			errors: [],
+		});
+	} catch (err) {
+		return res.status(500).send({
+			status_code: 500,
+			results: {},
+			errors: [err.message],
+		});
+	}
 }
 
 export async function getFiatController(
-  req: Request,
-  res: Response,
+	req: Request,
+	res: Response,
 ): Promise<Response> {
-  try {
-    const { name, symbol } = req.params;
+	try {
+		const { name, symbol } = req.params;
 
-    const fiat = await getFiat({
-      name,
-      symbol,
-    });
+		const fiat = await getFiat({
+			name,
+			symbol,
+		});
 
-    return res.status(200).send({
-      status_code: 200,
-      results: fiat,
-      errors: [],
-    });
-  } catch (err) {
-    return res.status(500).send({
-      status_code: 500,
-      results: {},
-      errors: [err.message],
-    });
-  }
+		return res.status(200).send({
+			status_code: 200,
+			results: fiat,
+			errors: [],
+		});
+	} catch (err) {
+		return res.status(500).send({
+			status_code: 500,
+			results: {},
+			errors: [err.message],
+		});
+	}
 }
 
 export async function createFiatsJSON(
-  req: Request,
-  res: Response,
+	req: Request,
+	res: Response,
 ): Promise<Response> {
-  try {
-    fiatsJson.forEach(async (fiat) => {
-      await createFiat({
-        name: fiat.name,
-        symbol: fiat.symbol,
-      });
-    });
+	try {
+		fiatsJson.forEach(async (fiat) => {
+			await createFiat({
+				name: fiat.name,
+				symbol: fiat.symbol,
+			});
+		});
 
-    return res.status(200).send({
-      status_code: 200,
-      results: {},
-      errors: [],
-    });
-  } catch (err) {
-    return res.status(500).send({
-      status_code: 500,
-      results: [],
-      errors: [err.message],
-    });
-  }
+		return res.status(200).send({
+			status_code: 200,
+			results: {},
+			errors: [],
+		});
+	} catch (err) {
+		return res.status(500).send({
+			status_code: 500,
+			results: [],
+			errors: [err.message],
+		});
+	}
 }
